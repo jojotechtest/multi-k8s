1f9f47bc72416abcac1b0d3e67c878455f432915
Jo <49952360+jojotechtest@users.noreply.github.com>
2019-05-09 17:26:22 +0200

    more configuration for Travis

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t jojotechtest/multi-client:latest -t jojotechtest/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t jojotechtest/multi-server:latest -t jojotechtest/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t jojotechtest/multi-worker:latest -t jojotechtest/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push jojotechtest/multi-client:latest
+docker push jojotechtest/multi-server:latest
+docker push jojotechtest/multi-worker:latest
+
+docker push jojotechtest/multi-client:$SHA
+docker push jojotechtest/multi-server:$SHA
+docker push jojotechtest/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/client-deployment client=jojotechtest/multi-client:$SHA
+kubectl set image deployments/server-deployment server=jojotechtest/multi-server:$SHA
+kubectl set image deployments/worker-deployment worker=jojotechtest/multi-worker:$SHA
